b2b28f85d971e086703c7039d4b9e296e4219db1
Vinod Akshat <pumkininriver@gmail.com>
2025-02-19 15:24:17 +0530

    meta tags

--- V2/portfolio/app/layout.tsx
+++ V2/portfolio/app/layout.tsx
@@ -13,18 +13,36 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Vinod Akshat",
-  description: "Portfolio website of Vinod Akshat",
+  title: "Vinod Akshat | Portfolio",
+  description: "Portfolio website of Vinod Akshat, showcasing projects, skills, and achievements.",
+  keywords: "Vinod Akshat, Vinod Akshat Writer,portfolio, web developer,Writer, software engineer, Stories,projects, resume",
+  metadataBase: new URL("https://vinod-akshat.vercel.app"), 
+  openGraph: {
+    title: "Vinod Akshat | Portfolio",
+    description: "Explore the projects, skills, and achievements of Vinod Akshat.",
+    url: "https://vinod-akshat.vercel.app",
+    siteName: "Vinod Akshat",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Vinod Akshat | Portfolio",
+    description: "Explore the projects, skills, and achievements of Vinod Akshat.",
+  },icons: {
+    icon: "/favicon.ico", 
+    shortcut: "/favicon.ico",
+    apple: "/apple-touch-icon.png",
+  },
+  other: {
+    "google-site-verification": "vHJoXYV__FsbiPx4JHobGDr4SFCvxgvuGKXudvlZXNg",
+  },
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
-
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
